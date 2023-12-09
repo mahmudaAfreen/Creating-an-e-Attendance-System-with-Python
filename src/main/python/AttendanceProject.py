f1eac88568602781b7e894ca495f61cab164ed3f
Mahmuda Akter <mafrin65@gmail.com>
2023-12-09 14:41:34 +0100

    Update AttendanceProject.py

--- src/main/python/AttendanceProject.py
+++ src/main/python/AttendanceProject.py
@@ -1,9 +1,8 @@
-
-import cv2  # Import the OpenCV library
-import numpy as np  # Import NumPy
-import face_recognition  # Import the face_recognition library
-import os  # Import the os library for file and directory management
-from datetime import datetime  # Import the datetime module for time-related functions
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
 
 # Set the path to the directory containing the known images
 path = 'ImagesAttendance'
@@ -28,45 +27,45 @@ for cl in myList:
 print(classNames)
 
 # Define a function to find the encodings for the known images
-def findencodings(photos):
+def find_encodings(photos):
     # Initialize an empty list for the encodings
-    cdata = []
+    encodings_list = []
     # Iterate through the list of images
-    for i in photos:
+    for img in photos:
         # Convert the image from BGR to RGB color space
-        i = cv2.cvtColor(i, cv2.COLOR_BGR2RGB)
+        rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         # Find the encoding for the image
-        encode = face_recognition.face_encodings(i)[0]
+        encode = face_recognition.face_encodings(rgb_img)[0]
         # Add the encoding to the list
-        cdata.append(encode)
+        encodings_list.append(encode)
     # Return the list of encodings
-    return cdata
+    return encodings_list
 
 # Define a function to mark attendance for a given name
-def markattendance(name):
+def mark_attendance(name):
     # Open the attendance file in read/write mode
     with open('Attendance.csv', 'r+') as f:
         # Read the contents of the file
-        mydata = f.readlines()
+        attendance_data = f.readlines()
         # Initialize an empty list for the names in the file
-        namelist = []
+        name_list = []
         # Iterate through the lines in the file
-        for line in mydata:
+        for line in attendance_data:
             # Split the line by the comma separator
             entry = line.split(',')
             # Add the name to the list
-            namelist.append(entry[0])
+            name_list.append(entry[0])
         # If the name is not in the list
-        if name not in namelist:
+        if name not in name_list:
             # Get the current time
             now = datetime.now()
             # Format the time as a string in the desired format
-            show = now.strftime('%H:%M:%S')
+            time_str = now.strftime('%H:%M:%S')
             # Write the name and time to the file, with a newline character
-            f.writelines(f'\n{name},{show}')
+            f.writelines(f'\n{name},{time_str}')
 
 # Find the encodings for the known images
-encodelistknown = findencodings(photos)
+encodings_known = find_encodings(photos)
 # Print a message indicating that the encoding is complete
 print('Encoding Complete')
 
@@ -77,35 +76,36 @@ while True:
     # Capture a frame from the webcam
     success, img = cap.read()
     # Resize the frame to a quarter of its original size
-    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    img_small = cv2.resize(img, (0, 0), None, 0.25, 0.25)
     # Convert the frame from BGR to RGB color space
-    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    rgb_frame = cv2.cvtColor(img_small, cv2.COLOR_BGR2RGB)
 
     # Find the locations of faces in the frame
-    facesCurFrame = face_recognition.face_locations(imgS)
+    faces_cur_frame = face_recognition.face_locations(rgb_frame)
     # Find the encodings of the faces in the frame
-    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+    encodes_cur_frame = face_recognition.face_encodings(rgb_frame, faces_cur_frame)
 
     # Iterate through the encodings and locations of the faces in the frame
-    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+    for encode_face, face_loc in zip(encodes_cur_frame, faces_cur_frame):
         # Calculate the distance between the current face encoding and the known encodings
-        faceDis = face_recognition.face_distance(encodelistknown, encodeFace)
+        face_distances = face_recognition.face_distance(encodings_known, encode_face)
         # Print the face distance
-        print(faceDis)
+        print(face_distances)
         # Get the index of the closest matching encoding
-        matchIndex = np.argmin(faceDis)
+        match_index = np.argmin(face_distances)
 
         # If the distance to the closest encoding is less than 0.6
-        if faceDis[matchIndex] < 0.60:
+        if face_distances[match_index] < 0.60:
             # Set the name to the corresponding class name
-            name = classNames[matchIndex].upper()
+            name = classNames[match_index].upper()
             # Mark attendance for the name
-            markattendance(name)
+            mark_attendance(name)
         else:
             # If the distance is greater than or equal to 0.6, set the name to "Unknown"
             name = 'Unknown'
+        
         # Get the coordinates of the face location
-        y1, x2, y2, x1 = faceLoc
+        y1, x2, y2, x1 = face_loc
         # Multiply the coordinates by 4 to scale them up from the resized frame
         y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
         # Draw a rectangle around the face
