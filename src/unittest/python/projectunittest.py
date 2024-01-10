97a3e46db8bacc3f2da927d218f128dee536fc52
Mahmuda Akter <mafrin65@gmail.com>
2024-01-10 20:57:30 +0100

    Update projectunittest.py

--- src/unittest/python/projectunittest.py
+++ src/unittest/python/projectunittest.py
@@ -7,23 +7,26 @@ import face_recognition
 from src.main.python.AttendanceProject import markattendance, cap, classNames, photos, findencodings
 import src.main.python.AttendanceProject
 
+# Define a constant for the test person's name
+TEST_PERSON_NAME = 'Test Person'
+
 class TestAttendanceMarking(unittest.TestCase):
     def setUp(self):
         # Set up test data by adding a known face to the list of photos
         self.test_image = cv2.imread('test.jpg')
         photos.append(self.test_image)
-        classNames.append('Test Person')
+        classNames.append(TEST_PERSON_NAME)
         self.encodelist_known = findencodings(photos)
 
     def test_markattendance(self):
         # Test that the markattendance function adds an entry to the Attendance.csv file
-        markattendance('Test Person')
+        markattendance(TEST_PERSON_NAME)
         with open('Attendance.csv', 'r') as file:
             lines = file.readlines()
             # Verify that there are 2 lines in the file (1 header, 1 entry)
             self.assertEqual(len(lines), 2)
             # Verify that the first field in the second line is 'Test Person'
-            self.assertEqual(lines[1].split(',')[0], 'Test Person')
+            self.assertEqual(lines[1].split(',')[0], TEST_PERSON_NAME)
 
     def test_face_recognition(self):
         # Test that the face recognition correctly identifies the known face
@@ -41,9 +44,9 @@ class TestAttendanceMarking(unittest.TestCase):
             face_dis = face_recognition.face_distance(self.encodelist_known, encode_face)
             match_index = np.argmin(face_dis)
             
-            # Assert that the distance is less than 0.60 and the name matches 'Test Person'
+            # Assert that the distance is less than 0.60 and the name matches the constant
             self.assertLess(face_dis[match_index], 0.60)
-            self.assertEqual(classNames[match_index], 'Test Person')
+            self.assertEqual(classNames[match_index], TEST_PERSON_NAME)
 
 if __name__ == '__main__':
     unittest.main()
